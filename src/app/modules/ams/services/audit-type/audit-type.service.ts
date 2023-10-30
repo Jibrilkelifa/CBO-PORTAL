3e8f51fdfcd74222074051126e986ec86ef52e90
abdyf <abdywak@gmail.com>
2023-10-30 08:37:43 +0300

    changed url

--- src/app/modules/ams/services/audit-type/audit-type.service.ts
+++ src/app/modules/ams/services/audit-type/audit-type.service.ts
@@ -15,7 +15,7 @@ export class AuditPlanService {
         Authorization: `Bearer ${localStorage.getItem('access_token')}`,
       }),
     };
-    this.apiServiceUrl = 'http://localhost:8099';
+    this.apiServiceUrl = 'http://10.1.125.58:8099';
   }
 
   constructor(private http: HttpClient) {}
