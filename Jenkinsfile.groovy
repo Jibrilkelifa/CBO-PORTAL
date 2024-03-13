659de715c1d0095817346caa8b3931de6748959a
abdyf <abdyf@coopbank.local>
2024-03-13 16:25:55 +0300

    jenkins test

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,61 @@
+pipeline {
+    agent any
+    
+    stages {
+        stage('Check Dependencies') {
+            steps {
+                // Check if node_modules directory exists
+                script {
+                    if (!fileExists('node_modules')) {
+                        // If node_modules doesn't exist, run npm install
+                        echo 'node_modules directory does not exist. Running npm install...'
+                        sh 'npm install'
+                    } else {
+                        // If node_modules exists, check if package-lock.json has changed
+                        def previousPackageLock = readFileIfExists('package-lock.json')
+                        sh 'git checkout HEAD package-lock.json'
+                        def currentPackageLock = readFileIfExists('package-lock.json')
+
+                        if (currentPackageLock != previousPackageLock) {
+                            echo 'package-lock.json has changed. Running npm install...'
+                            sh 'npm install'
+                        } else {
+                            echo 'No changes in package-lock.json. Skipping npm install...'
+                        }
+                    }
+                }
+            }
+        }
+        
+        stage('Build') {
+            steps {
+                // Build Angular project in production mode
+                sh 'npm run build -- --prod'
+            }
+        }
+    }
+    
+    post {
+        success {
+            // Add deployment steps here (e.g., deploying to a production server)
+            echo 'Build successful! Deploying to production...'
+            // Example deployment command (replace with your deployment script)
+            sh 'scp -r dist/* user@production-server:/path/to/deployment/directory'
+        }
+        failure {
+            echo 'Build failed!'
+        }
+    }
+}
+
+def fileExists(filePath) {
+    return file(filePath).exists()
+}
+
+def readFileIfExists(filePath) {
+    if (fileExists(filePath)) {
+        return readFile(filePath).trim()
+    } else {
+        return ''
+    }
+}
