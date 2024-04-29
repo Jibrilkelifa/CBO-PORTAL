0a6674eacaab1e25e77a5fa431360f4e5317db7d
Dame <demechelsea@gmail.com>
2024-04-29 15:10:35 +0300

    end point changed for finance

--- src/app/modules/icms/finance/service/finance-services.service.ts
+++ src/app/modules/icms/finance/service/finance-services.service.ts
@@ -17,7 +17,9 @@ export class FinanceService {
         Authorization: `Bearer ${localStorage.getItem('access_token')}`,
       }),
     };
-    this.apiServiceUrl = 'http://localhost:8084';
+    //this.apiServiceUrl = 'http://localhost:8084';
+    this.apiServiceUrl = 'http://10.1.125.58:8084';
+
   }
 
   constructor(private http: HttpClient) { }
